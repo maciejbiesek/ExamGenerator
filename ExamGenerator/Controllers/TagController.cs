ea4cc584d17c75c82835e6ddaf505f9bdb96e673
monic <mon@NTbook>
2014-09-04 15:11:52 +0200

    Add connection to database and controllers

---
+++ ExamGenerator/Controllers/TagController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ExamGenerator.Models;
+
+namespace ExamGenerator.Controllers
+{
+    public class TagController : Controller
+    {
+        private generator_kolokwiowEntities genKolEnt = new generator_kolokwiowEntities();
+
+        public ActionResult Index()
+        {
+            List<TAGS> getTags = genKolEnt.TAGS.Select(t => t).ToList();
+            return View(getTags);
+        }
+
+    }
+}
---
+++ ExamGenerator/Controllers/TaskController.cs
@@ -0,0 +1,21 @@
+﻿using ExamGenerator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExamGenerator.Controllers
+{
+    public class TaskController : Controller
+    {
+        private generator_kolokwiowEntities genKolEnt = new generator_kolokwiowEntities();
+
+        public ActionResult Index()
+        {
+            List<TASKS> getTasks = genKolEnt.TASKS.Select(t => t).ToList();
+            return View(getTasks);
+        }
+
+    }
+}
