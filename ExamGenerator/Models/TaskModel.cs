ed47d3b5ffd17788e83f8cad21cd579913f7bd50
maciejbiesek <maciejbiesek@gmail.com>
2014-09-04 15:31:04 +0200

    model potrzebny do edytowania tasków

---
+++ ExamGenerator/Models/TaskModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExamGenerator.Models
+{
+    public class TaskModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int IdTag { get; set; }
+        public string TagName { get; set; }
+    }
+}
