77ad5cb09ad28b50e83032232c7df7a974974079
maciejbiesek <maciejbiesek@gmail.com>
2014-09-05 14:57:12 +0200

    Revert "add create task action"
    This reverts commit f6f8fef88d43017583ebbbf8cf76d01d2bc33294.

--- ExamGenerator/Models/TaskModel.cs
+++ ExamGenerator/Models/TaskModel.cs
@@ -2,18 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Mvc;
 
 namespace ExamGenerator.Models
 {
     public class TaskModel
     {
-
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Content { get; set; }
         public int IdTag { get; set; }
         public string TagName { get; set; }
-
     }
 }
